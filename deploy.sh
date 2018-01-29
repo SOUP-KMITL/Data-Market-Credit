3e5d4f5e100cc66cdc265f517a612985b3200b3a
Kohpai <whcwhc78@gmail.com>
2018-01-29 17:09:56 +0700

    update internal container address

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@
 if [ -z "$1" ] || [ -z "$2" ]
 then
     echo "rsync -azrP --exclude-from \"rsync.exc\" --include \".dockerignore\" \
-        ./ smartCity-dataMarket-tmp:/home/centos/kohpai/CreditService/"
+        ./ api-smartCity:/home/centos/kohpai/CreditService/"
     rsync -azrP --exclude-from "rsync.exc" --include ".dockerignore" \
-        ./ smartCity-dataMarket-tmp:/home/centos/kohpai/CreditService/
+        ./ api-smartCity:/home/centos/kohpai/CreditService/
 else
     echo "rsync -azrP --exclude-from \"rsync.exc\" --include \".dockerignore\" ./ $1:$2"
     rsync -azrP --exclude-from "rsync.exc" --include ".dockerignore" ./ $1:$2
